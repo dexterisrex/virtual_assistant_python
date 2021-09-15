5ad2840a2cf62a026fd09fdb462b2af7c690555e
@dexterisrex <rahulsharma.sharma931@gmail.com>
2021-09-15 13:51:12 +0530

    Giving command to assistant

---
+++ main.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print("Listening...")
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            print(command)
+except:
+    print("Microphone doesn't work")
